adf57b725390e012bbb8ae0cd01cc511e8649fff
sanchez005909 <sanchez005909@gmail.com>
2023-08-09 09:25:40 +0300

    fixed bugs

--- src/channel.py
+++ src/channel.py
@@ -12,7 +12,7 @@ class Channel:
         channel = Channel.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         self.title = channel["items"][0]["snippet"]["title"]
         self.description = channel["items"][0]["snippet"]["description"]
-        self.url = 'https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A'
+        self.url = f'https://www.youtube.com/channel/{self.__channel_id}]'
         self.subscriber_count = channel["items"][0]["statistics"]["subscriberCount"]
         self.video_count = channel["items"][0]["statistics"]["videoCount"]
         self.view_count = channel["items"][0]["statistics"]["viewCount"]
